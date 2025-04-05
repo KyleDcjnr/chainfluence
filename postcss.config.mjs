3873a9636350566fee78e484166be9aa9defde37
victor <osujidcnr@gmail.com>
2025-04-05 18:51:13 +0100

    updates

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,13 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme: {
+    extend: {
+      colors: {
+        'lemon-green': '#ccff33', // Or your exact lemon green hex
+      },
+    },
+  },
+
 };
 
 export default config;
